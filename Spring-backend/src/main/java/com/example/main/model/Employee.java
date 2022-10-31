e93f198c7fa886e62c41f84860eabb22d52e7131
Somayeh Karami <sk20206@yahoo.com>
2022-10-31 15:30:34 -0400

    Added tne Entity and images and uml

---
+++ Spring-backend/src/main/java/com/example/main/model/Employee.java
@@ -0,0 +1,61 @@
+package com.example.main.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+    @Column(name = "first_name")
+	private String firstNmae;
+    @Column(name = "last_name")
+	private String lastName;
+    @Column(name = "email_id")
+	private String emailId;
+	
+	
+	
+	
+	public Employee() {
+		super();
+	}
+	public Employee(String firstNmae, String lastName, String emailId) {
+		super();
+		this.firstNmae = firstNmae;
+		this.lastName = lastName;
+		this.emailId = emailId;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstNmae() {
+		return firstNmae;
+	}
+	public void setFirstNmae(String firstNmae) {
+		this.firstNmae = firstNmae;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+	
+}
